7a27b948cd16621ea2dbd535dcf5dd56723616ca
ShiningElektra <elektrathanos@googlemail.com>
2021-02-13 09:54:16 +0100

    🎉 Add Hero Files and html look-a-like

---
+++ src/components/superheroes/heroes.ts
@@ -0,0 +1,22 @@
+// <div class="game">
+//     <article class="leftcard">
+//         <div class="player1">NamePlayer1
+//           <div class="card__front">PicHero
+//             <h2 class="hero">NameHero</h2>
+//              <div class="card__info">Powers
+//              </div>
+//             </div>
+//         </div>
+//     </article>
+//     <article class="rightcard">
+//         <div class="player2">NamePlayer2
+//           <div class="card__front">PicHeroe
+//             <h2 class="hero">NameHero</h2>
+//              <div class="card__info">Powers
+//              </div>
+//              </div>
+//          </div>
+//      </article>
+//     <div class="card__back">Flip who won
+//     </div>
+// </div>
